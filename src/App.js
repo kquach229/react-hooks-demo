fb0f276e9321dc7a20860246f73f4e9bcca58830
kquach229 <kquach229>
2023-12-28 14:43:38 -0500

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import './App.css'
-import ContextProvider from './context/ContextProvider'
+import { useReducer } from 'react';
+import './App.css';
+import ContextProvider from './context/ContextProvider';
 
-import Header from './header'
+import Header from './header';
 
 function App() {
+  const counterReducer = (state, action) => {
+    switch (action.type) {
+      case 'DECREMENT':
+        return { count: state.count - 1 };
+      case 'INCREMENT':
+        return { count: state.count + 1 };
+      case 'RESET':
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
+  const [counter, dispatch] = useReducer(counterReducer, { count: 0 });
   return (
     // wrap app in context provider
     <ContextProvider>
-      <div className="App">
+      <div className='App'>
         <Header />
+        <button onClick={() => dispatch({ type: 'DECREMENT' })}>
+          Decrement
+        </button>
+        count: {counter.count}
+        <button onClick={() => dispatch({ type: 'INCREMENT' })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
       </div>
     </ContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
